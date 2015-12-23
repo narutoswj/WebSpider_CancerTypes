ee1091d84fa227373b755312434351104c82c2de
narutoswj <narutoswj@hotmail.com>
2015-12-23 23:26:23 +0800

    Add ScrapySharp

--- WebSpider_CancerTypes/Program.cs
+++ WebSpider_CancerTypes/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
+using ScrapySharp.Extensions;
+using ScrapySharp.Network;
+
 
 namespace WebSpider_CancerTypes
 {
@@ -10,6 +14,48 @@ namespace WebSpider_CancerTypes
     {
         static void Main(string[] args)
         {
+            //string html = GetUrltoHtml("http://www.cancer.gov/", "utf-8");
+            //Console.WriteLine(html);
+            //Console.ReadLine();
+
+            var uri = new Uri("http://www.cancer.gov/");
+            var browser = new ScrapingBrowser();
+            var htmlDoc = browser.DownloadString(uri);
+
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(htmlDoc);
+
+            var htmlNode = htmlDocument.DocumentNode;
+            var title = htmlNode.CssSelect("title");
+
+            foreach (var htmlNod in title)
+            {
+                Console.WriteLine(htmlNode.InnerHtml);
+            }
+
+        }
+
+
+
+        public static string GetUrltoHtml(string Url, string type)
+        {
+            try
+            {
+                System.Net.WebRequest wReq = System.Net.WebRequest.Create(Url);
+                // Get the response instance.
+                System.Net.WebResponse wResp = wReq.GetResponse();
+                System.IO.Stream respStream = wResp.GetResponseStream();
+                // Dim reader As StreamReader = New StreamReader(respStream)
+                using (System.IO.StreamReader reader = new System.IO.StreamReader(respStream, Encoding.GetEncoding(type)))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+            catch (System.Exception ex)
+            {
+                return ex.Message;
+            }
+
         }
     }
 }
